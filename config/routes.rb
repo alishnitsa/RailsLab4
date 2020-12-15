33ddd30949ddbe63af92a395c63135095a8fd8b6
alish <alish.gilemov@mail.ru>
2020-12-15 18:42:36 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  get 'welcome/index'
-  root 'welcome#index'
+#  get 'welcome/index'
+#  root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
